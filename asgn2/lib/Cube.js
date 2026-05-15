934366dacb7a9c4530cd0f090e50061837310ac4
dinhhannah <hannahdinhHD@gmail.com>
2026-05-14 23:03:17 -0700

    adding changes

--- asgn2/lib/Cube.js
+++ asgn2/lib/Cube.js
@@ -2,13 +2,34 @@ class Cube{
    constructor(){
         this.type = "cube";
         // this.position = [0.0, 0.0, 0.0];  
-        this.color = [1.0, 1.0, 1.0, 1.0];
+        this.color = [0.6, 0.6, 0.6, 1.0];
         //this.size = 5.0;
         //this.segments = 10;
         this.matrix = new Matrix4();
+        this.vBuffer = null;
+        this.UVBuffer = null;
+        this.textureOption = [0,0,0,0,0,0]; //array of texture options
     }
 
    render() {
+        if(this.vBuffer === null) {
+            this.vBuffer = gl.createBuffer();
+            
+            if (!this.vBuffer) {
+              console.log("Failed to create the buffer object");
+              return -1;
+            }
+        }
+
+        if(this.UVBuffer === null) {
+            this.UVBuffer = gl.createBuffer();
+            
+            if (!this.UVBuffer) {
+              console.log("Failed to create the buffer object");
+              return -1;
+            }
+        }
+
         var rgba = this.color;
 
         // Pass the color of a point to u_FragColor uniform variable
@@ -46,5 +67,105 @@ class Cube{
         gl.uniform4f(u_FragColor, rgba[0]*0.5, rgba[1]*0.5, rgba[2]*0.5, rgba[3]);
         drawTriangle3D([0,0,0, 1,0,1, 1,0,0]);
         drawTriangle3D([0,0,0, 0,0,1, 1,0,1]);
+    }
+
+    renderFast() {
+      let rgba = this.color;
+      gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+      gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+
+      let allVerts = []
+      
+      // front faces
+      gl.uniform1i(u_whichTexture, 0);
+      allVerts = allVerts.concat([0.0, 0.0, 0.0,  1.0, 1.0, 0.0,  1.0, 0.0, 0.0]);
+      allVerts = allVerts.concat([0.0, 0.0, 0.0,  0.0, 1.0, 0.0,  1.0, 1.0, 0.0]);
+        
+      //back face
+      allVerts = allVerts.concat([1.0, 0.0, 1.0,  0.0, 1.0, 1.0,  0.0, 0.0, 1.0]);
+      allVerts = allVerts.concat([1.0, 0.0, 1.0,  1.0, 1.0, 1.0,  0.0, 1.0, 1.0]);
+
+      // right face
+      allVerts = allVerts.concat([1.0, 0.0, 0.0,  1.0, 1.0, 1.0,  1.0, 0.0, 1.0]);
+      allVerts = allVerts.concat([1.0, 0.0, 0.0,  1.0, 1.0, 0.0,  1.0, 1.0, 1.0]);
+
+      //left face
+      allVerts = allVerts.concat([0.0, 0.0, 1.0,  0.0, 1.0, 0.0,  0.0, 0.0, 0.0]);
+      allVerts = allVerts.concat([0.0, 0.0, 1.0,  0.0, 1.0, 1.0,  0.0, 1.0, 0.0]);
+
+      // top face
+      allVerts = allVerts.concat([0.0, 1.0, 0.0,  1.0, 1.0, 1.0,  1.0, 1.0, 0.0]);
+      allVerts = allVerts.concat([0.0, 1.0, 0.0,  0.0, 1.0, 1.0,  1.0, 1.0, 1.0]);
+
+      //Bottom face
+      allVerts = allVerts.concat([0.0, 0.0, 1.0,  1.0, 0.0, 0.0,  1.0, 0.0, 1.0]);
+      allVerts = allVerts.concat([0.0, 0.0, 1.0,  0.0, 0.0, 0.0,  1.0, 0.0, 0.0]);
+
+      drawTriangle3D(allVerts);
+    }
+
+    renderSkybox() {
+        if(this.vBuffer === null) {
+            this.vBuffer = gl.createBuffer();
+            
+            if (!this.vBuffer) {
+              console.log("Failed to create buffer object");
+              return -1;
+            }
+        }
+
+        if(this.UVBuffer === null) {
+            this.UVBuffer = gl.createBuffer();
+            
+            if (!this.UVBuffer) {
+              console.log("Failed to create buffer object");
+              return -1;
+            }
+        }
+
+        var rgba = this.color;
+
+        gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+
+        // front of  cube
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+        gl.uniform1i(u_whichTexture, this.textureOption);
+        
+        // front face
+        gl.uniform1i(u_whichTexture, this.textureOption[0]);
+        drawTriangle3DUV(this.vBuffer, this.UVBuffer, [1.0, 0.0, 0.0,  0.0, 1.0, 0.0,  0.0, 0.0, 0.0], [0,0,     1,1,     1,0]);
+        drawTriangle3DUV(this.vBuffer, this.UVBuffer, [1.0, 0.0, 0.0,  1.0, 1.0, 0.0,  0.0, 1.0, 0.0], [0,0,     0,1,     1,1]);
+        
+        // back face
+        gl.uniform1i(u_whichTexture, this.textureOption[1]);
+        drawTriangle3DUV(this.vBuffer, this.UVBuffer, [0.0, 0.0, 1.0,  1.0, 1.0, 1.0,  1.0, 0.0, 1.0], [0,0,     1,1,     1,0]);
+        drawTriangle3DUV(this.vBuffer, this.UVBuffer, [0.0, 0.0, 1.0,  0.0, 1.0, 1.0,  1.0, 1.0, 1.0], [0,0,     0,1,     1,1]);
+
+        // right face
+        gl.uniform1i(u_whichTexture, this.textureOption[2]);
+        drawTriangle3DUV(this.vBuffer, this.UVBuffer, [1.0, 0.0, 1.0,  1.0, 1.0, 0.0,  1.0, 0.0, 0.0], [0,0,     1,1,     1,0]);
+        drawTriangle3DUV(this.vBuffer, this.UVBuffer, [1.0, 0.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 0.0], [0,0,     0,1,     1,1]);
+
+        // left face
+        gl.uniform1i(u_whichTexture, this.textureOption[3]);
+        drawTriangle3DUV(this.vBuffer, this.UVBuffer, [0.0, 0.0, 0.0,  0.0, 1.0, 1.0,  0.0, 0.0, 1.0], [0,0,     1,1,     1,0]);
+        drawTriangle3DUV(this.vBuffer, this.UVBuffer, [0.0, 0.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 1.0], [0,0,     0,1,     1,1]);
+
+
+        // top face
+        gl.uniform1i(u_whichTexture, this.textureOption[4]);
+        drawTriangle3DUV(this.vBuffer, this.UVBuffer, [1.0, 1.0, 0.0,  0.0, 1.0, 1.0,  0.0, 1.0, 0.0], [0,0,     1,1,     1,0]);
+        drawTriangle3DUV(this.vBuffer, this.UVBuffer, [1.0, 1.0, 0.0,  1.0, 1.0, 1.0,  0.0, 1.0, 1.0], [0,0,     0,1,     1,1]);
+
+        // bottom face
+        gl.uniform1i(u_whichTexture, this.textureOption[5]);
+        drawTriangle3DUV(this.vBuffer, this.UVBuffer, [0.0, 0.0, 0.0,  1.0, 0.0, 1.0,  1.0, 0.0, 0.0], [0,0,     1,1,     1,0]);
+        drawTriangle3DUV(this.vBuffer, this.UVBuffer, [0.0, 0.0, 0.0,  0.0, 0.0, 1.0,  1.0, 0.0, 1.0], [0,0,     0,1,     1,1]);
+
+        gl.deleteBuffer(this.vBuffer);
+        gl.deleteBuffer(this.UVBuffer);
+
     }
 }
--- asgn2/lib/Triangle.js
+++ asgn2/lib/Triangle.js
@@ -4,6 +4,9 @@ class Triangle {
         this.position = [0.0, 0.0, 0.0];
         this.color = [1.0, 1.0, 1.0, 1.0];
         this.size = 5.0;
+
+        this.buffer = null;
+        this.vertices = null;
     }
 
     render() {
@@ -13,80 +16,88 @@ class Triangle {
     
         // Pass the position of a point to a_Position variable
         // gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-
         // Pass the color of a point to u_FragColor variable
         gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
     
         gl.uniform1f(u_Size, size);
+    
+        // Draw
+        // gl.drawArrays(gl.POINTS, 0, 1);
+        let delta = this.size/200.0;
+        drawTriangle([xy[0], xy[1], xy[0] - delta/2, xy[1] - delta*Math.sqrt(3)/2, xy[0] + delta/2, xy[1] - delta*Math.sqrt(3)/2]);
+    }
+
+    render3D() {
+      var n = 3;
+      
+      var xy = this.position;
+      var rgba = this.color;
+      var size = this.size;
+
+      gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+      if(this.vertices === null) {
+        console.log("Failed to find vertices for triangle");
+        return -1;
+      }
+
+      if(this.buffer === null) {
+        this.buffer = gl.createBuffer();
         
-        // If position array has 6 values, it's a custom triangle (generate picture)
-        if (this.position.length == 6) {
-            drawTriangle(this.position);
-        } 
-
-        //else it's a single point click, so calculate the vertices based on size
-        else {
-            // Draw
-            // gl.drawArrays(gl.POINTS, 0, 1);
-            let xy = this.position;
-            let d = this.size / 200.0;
-            drawTriangle([
-                xy[0], xy[1],
-                xy[0] + d, xy[1],
-                xy[0], xy[1] + d
-            ]);  
+        if (!this.buffer) {
+          console.log("Failed to create the buffer object");
+          return -1;
         }
-    }
-}
+      }
 
-function drawTriangle(vertices) {
-    var n = 3; // The number of vertices
-  
-    // Create a buffer object
-    var vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-      console.log('Failed to create the buffer object');
-      return -1;
+      drawTriangle3D(this.vertices, this.buffer);
     }
-  
-    // Bind the buffer object to target
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    // Write data into the buffer object
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
-
-    // Assign the buffer object to a_Position variable
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-  
-    // Enable the assignment to a_Position variable
-    gl.enableVertexAttribArray(a_Position);
-  
-    gl.drawArrays(gl.TRIANGLES, 0, n);
 }
 
 function drawTriangle3D(vertices) {
-    if (arguments.length > 1) {
-        vertices = Array.from(arguments);
-    }
-    
-    var n = 3; // The number of vertices
-  
-    // Create a buffer object
-    var vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-      console.log('Failed to create the buffer object');
-      return -1;
-    }
-  
-    // Bind the buffer object to target
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    // Write data into the buffer object
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+  var n = vertices.length/3; // The number of vertices
 
-    // Assign the buffer object to a_Position variable
-    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+  let vBuffer = gl.createBuffer();
+  if (!vBuffer) {
+    console.log("Failed to create the vBuffer object");
+    return -1;
+  }
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  // Write date into the buffer object
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+
+  // Assign the buffer object to a_Position variable
+  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_Position);
+
+  gl.drawArrays(gl.TRIANGLES, 0, n);
+}
+
+// built for interleaved buffers
+function drawTriangle3DUV(vBuffer, UVBuffer, vertices, uv) {
+  var n = 3; // The number of vertices
+
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+
+  // let FSIZE = vertices.BYTES_PER_ELEMENT;
+
+  // // interleaved with color and coords (color is indices 3-5)
+  // gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, FSIZE*6, 0);
   
-    // Enable the assignment to a_Position variable
-    gl.enableVertexAttribArray(a_Position);
+  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Position);
+
+  // uv coords
+  gl.bindBuffer(gl.ARRAY_BUFFER, UVBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uv), gl.DYNAMIC_DRAW);
   
-    gl.drawArrays(gl.TRIANGLES, 0, n);
+  gl.vertexAttribPointer(a_UV, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_UV);
+
+  gl.drawArrays(gl.TRIANGLES, 0, n);
 }
